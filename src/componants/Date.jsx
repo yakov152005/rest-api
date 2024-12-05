b17e59dde6805d235eb2f0299e75981c9f269999
yakov152005 <yakov152005@gmail.com>
2024-12-05 18:44:15 +0200

    הוספתי שינויים חדשים

--- src/componants/Date.jsx
+++ src/componants/Date.jsx
@@ -1,7 +1,15 @@
-export default function Date(props){
-    return(
+export default function Date(props) {
+    const DATE = props.date;
+
+    let [year, month, day] = DATE.split('/');
+    let FULL_DATE = `${day}/${month}/${year}`;
+
+    return (
         <div className="date">
-            <div>{props.date}</div>
+            <div><b>
+                <mark>Released date:</mark>
+                {FULL_DATE}
+            </b></div>
         </div>
     )
 }
--- src/componants/Item.jsx
+++ src/componants/Item.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Date from "./Date";
 
-export default function  Item (props) {
+export default function Item(props) {
 
     return (
         <div className="item">
-            <div className="item_name" >Pokemon: {props.name}</div>
+            <div className="item_details">
+                <div className="item_name" ><mark>Pokemon:</mark> {props.name}</div>
+                <Date date={props.date} />
+            </div>
             <img className="item_img" src={props.image} alt={props.name} />
-            <Date
-                date={props.date}
-                />
         </div>
-    )
+    );
 }
+
